2c5aba0d32b6b23c4585f263e02f07bfba694f18
yuki3738 <y.minamiya@gmail.com>
2019-09-24 23:55:15 +0900

    Add itp1_6_b.rb

---
+++ courses/lesson/2/ITP1/6/ITP1_6_B.rb
@@ -0,0 +1,14 @@
+PATTERNS = %w(S H C D).freeze
+n = gets.to_i
+cards = []
+
+n.times do
+  cards << gets.chomp
+end
+
+PATTERNS.each do |pattern|
+  1.upto(13) do |i|
+    card = "#{pattern} #{i}"
+    puts card unless cards.include?(card)
+  end
+end
